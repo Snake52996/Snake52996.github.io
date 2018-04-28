81dba42abf73974ffb1444f9bad5d81f2b0e42cb
Snake52996 <haoxuan0222@163.com>
2018-04-28 22:14:01 +0800

    Site updated: 2018-04-28 22:13:54

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
+/*$(document).ready(function () {*/
+$(window).load(function () {
 
-  var sidebarInner = $('.sidebar-inner');
+  /*var sidebarInner = $('.sidebar-inner');
+  var hitokoto = $('.sidebar-inner2');*/
+  
+  var sidebarInner = $("#base1");
+  var hitokoto = $("#target1");
 
   initAffix();
   resizeListener();
@@ -8,19 +13,50 @@ $(document).ready(function () {
   function initAffix () {
     var headerOffset = getHeaderOffset(),
         footerOffset = getFooterOffset(),
-        sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight(),
-        contentHeight = $('#content').height();
+        sidebarHeight = $('#sidebar').outerHeight(true) + NexT.utils.getSidebarb2tHeight(),
+		hitokotoHeight = $(hitokoto).outerHeight(true);
+        contentHeight = $('#content').outerHeight(true);
+		
+		/*
+		alert(headerOffset);	//456
+		alert(footerOffset);	//164
+		alert(sidebarHeight);	//496
+		alert(hitokotoHeight);	//56
+		alert(contentHeight);	//5590
+		*/
 
     // Not affix if sidebar taller then content (to prevent bottom jumping).
     if (headerOffset + sidebarHeight < contentHeight) {
+		//alert("!!");
       sidebarInner.affix({
         offset: {
           top: headerOffset - CONFIG.sidebar.offset,
           bottom: footerOffset
         }
       });
+	  hitokoto.affix({
+        offset: {
+          top: headerOffset - CONFIG.sidebar.offset,
+          bottom: footerOffset
+        }
+      });
     }
-
+	$(hitokoto).offset({top: $(hitokoto).offset().top + $(sidebarInner).outerHeight(true) + 10});
+	
+	/*if($(hitokoto).offset().top > $(sidebarInner).offset().top + $(sidebarInner).outerHeight(true) + 10) {
+		$(hitokoto).offset({top: $(sidebarInner).offset().top + $(sidebarInner).outerHeight(true) + 10});
+	}*/
+	
+	/*
+	if (headerOffset + sidebarHeight < contentHeight - hitokotoHeight) {
+	  hitokoto.affix({
+        offset: {
+          top: headerOffset + sidebarHeight - CONFIG.sidebar.offset,
+          bottom: footerOffset
+        }
+      });
+    }
+	*/
     setSidebarMarginTop(headerOffset).css({ 'margin-left': 'initial' });
   }
 
@@ -51,7 +87,8 @@ $(document).ready(function () {
   function recalculateAffixPosition () {
     $(window).off('.affix');
     sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
-    initAffix();
+    hitokoto.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+	initAffix();
   }
 
 });
